86a72bf8b4b9b26d38e93eaf30aca37dfa66f23d
Chintan Radia <radiachints1512@gmail.com>
2016-01-28 12:24:35 +0000

    add support for :heart_eyes:

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ import pathExists from 'path-exists';
 import fn from './';
 
 test('meme', t => {
-	await fn('Don\t forget to be awesome', {delay: 500, background: '#1d2628', filename: 'out.gif'});
+	await fn('Don\'t forget to be awesome', {delay: 500, background: '#1d2628', filename: 'out.gif'});
 
 	t.true(pathExists.sync('out.gif'));
 });
